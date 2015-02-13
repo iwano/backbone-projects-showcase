3b8c34c4e5bdbbed39a5f27e40843aaede39e743
Ivan Calderon <iwano@hotmail.com>
2015-02-12 18:45:17 -0600

    Fix test runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,6 +130,7 @@ module.exports = function (grunt) {
                         '<%= yeoman.app %>/bower_components/jquery/dist/jquery.js',
                         '<%= yeoman.app %>/bower_components/lodash/dist/lodash.js',
                         '<%= yeoman.app %>/bower_components/backbone/backbone.js',
+                        '<%= yeoman.app %>/bower_components/handlebars/handlebars.js',
                         '.tmp/scripts/templates.js'
                     ]
                 }
